633e45128fe9097a25c9a64125c0998419f142ea
kunal <kunal123jamdade@gmail.com>
2017-06-20 18:00:04 +0530

    Modified templates file and learned dynamic data

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'Blog/post_list.html')
+    author_name = User.objects.get(username='kunal')
+    posts_author_name = Post.objects.filter(author= author_name)#.order_by('published_date')
+    text_data = Post.objects.values().order_by('text')
+    return render(request, 'Blog/post_list.html', {'posts_author_name': posts_author_name,'text_data': text_data})
