46aa351092c6c09a9af2f81a4379da53947d491b
kunal <kunal123jamdade@gmail.com>
2017-06-24 16:27:39 +0530

    added view and template for detailed blog post as well as CSS for the site.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,4 +10,10 @@ def post_list(request):
     author_name = User.objects.get(username='kunal')
     posts_author_name = Post.objects.filter(author= author_name)#.order_by('published_date')
     text_data = Post.objects.values().order_by('text')
-    return render(request, 'Blog/post_list.html', {'posts_author_name': posts_author_name,'text_data': text_data})
+    return render(request, 'Blog/post_list.html', {'posts_author_name': posts_author_name})
+
+
+def post_detail(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'Blog/post_detail.html', {'post': post})
